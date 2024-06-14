c4866209b02002b6b82e8de2a98603a7cb61d7f8
pudinmah <pudinmah69@gmail.com>
2024-06-14 10:26:24 +0700

    assets & header

---
+++ android/app/src/main/kotlin/com/example/flutter_cakery/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_cakery
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/widget/navbar_widget.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class NavbarWidget extends StatelessWidget {
+  const NavbarWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomAppBar(
+      
+    );
+  }
+}
