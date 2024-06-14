51f731c13e9e89b9763fd73998c291ebd200ac08
pudinmah <pudinmah69@gmail.com>
2024-06-14 10:54:14 +0700

    home header and Bottom

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_cakery/widget/navbar_widget.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -12,8 +13,41 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Mahpudin Cakery'),
+        backgroundColor: Colors.white,
+        elevation: 0.0,
+        centerTitle: true,
+        leading: IconButton(
+          icon: const Icon(
+            Icons.arrow_back,
+            color: Color(0xFF545D68),
+          ),
+          onPressed: () {},
+        ),
+        title: const Text(
+          'Mahpudin Cakery 🍰',
+          style: TextStyle(
+            fontFamily: 'Varela',
+            fontSize: 24.0,
+            color: Color(0xFF545D68),
+          ),
+        ),
+        actions: [
+          IconButton(
+            icon: const Icon(
+              Icons.notifications_none,
+              color: Color(0xFF545D68),
+            ),
+            onPressed: () {},
+          ),
+        ],
       ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: const Color(0xFFF17532),
+        child: const Icon(Icons.fastfood),
+      ),
+      bottomNavigationBar: const NavbarWidget(),
     );
   }
 }
